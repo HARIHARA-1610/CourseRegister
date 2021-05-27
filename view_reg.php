ee89e113e6f45234d5155b528c5019afb7087a31
HARIHARA-1610 <63394910+HARIHARA-1610@users.noreply.github.com>
2021-05-27 17:44:54 +0530

    Update view_reg.php

--- view_reg.php
+++ view_reg.php
@@ -9,10 +9,10 @@
 session_start();
 
 
-$servername = "localhost";
-$username = "HariHara";
-$password = "Hari@12";
-$database = "course_reg";
+$servername = "remotemysql.com";
+$username = "yt8u76VtRK";
+$password = "bcXvcwYCWA";
+$database = "yt8u76VtRK";
 
 // Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
